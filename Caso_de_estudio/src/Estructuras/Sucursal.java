49603460b1c9f33268844a7aba9dc3415d3131e9
diego-kleinman <52840120+diego-kleinman@users.noreply.github.com>
2020-05-21 17:33:35 -0300

    Avance

--- Caso_de_estudio/src/Estructuras/Sucursal.java
+++ Caso_de_estudio/src/Estructuras/Sucursal.java
@@ -11,7 +11,15 @@ public class Sucursal implements ISucursal {
     private final String barrio;
     private final String ciudad;
     private final TArbolBB<Producto> arbolProductos;
-
+    
+    /**
+     * Constructor de la clase sucursal
+     * @param direccion Direccion
+     * @param telefono Teléfono
+     * @param nombre Nombre
+     * @param barrio Barrio
+     * @param ciudad Ciudad
+     */
     public Sucursal(String direccion, String telefono, String nombre, String barrio, String ciudad) {
         this.arbolProductos = new TArbolBB<>();
         this.nombre = nombre;
@@ -56,12 +64,12 @@ public class Sucursal implements ISucursal {
     @Override
     public void insertarProducto(Producto unProducto) {
 
-        // Instanciamos un producto auxiliar a partir de la etiqueta (código) del
+        // Instancio un producto auxiliar a partir de la etiqueta (código) del
         // producto recibido como parámetro
         // En caso de no existir quedará null.
-        IProducto prodAux = buscarPorCodigo(unProducto.getEtiqueta());
+        Producto prodAux = buscarPorCodigo(unProducto.getEtiqueta());
 
-        // Si no encontramos el producto en la lista, lo incorporamos a la sucursal
+        // Si no encuentro el producto en la lista, lo incorporo a la sucursal
         if (prodAux == null) {
             TElementoAB<Producto> aux = new TElementoAB<>(unProducto.getEtiqueta(), unProducto);
             this.arbolProductos.insertar(aux);
@@ -69,11 +77,13 @@ public class Sucursal implements ISucursal {
 
     }
 
-    // Agrega stock a un producto ya existente en el almacen
     @Override
     public Boolean agregarStock(Comparable etiqueta, Integer cantidad) {
+        
+        //Busco el producto
         TElementoAB<Producto> elem = getArbolProductos().buscar(etiqueta);
-
+        
+        //Si no lo encuentro no hago nada, si lo encuentro asigno su stock al que tenía antes + el que le quiero agregar
         if (elem == null) {
             return false;
         } else {
--- Caso_de_estudio/src/Interfaces/ISucursal.java
+++ Caso_de_estudio/src/Interfaces/ISucursal.java
@@ -8,57 +8,57 @@ public interface ISucursal {
     /**
      * Retorna la direccion de la sucursal
      *
-     * @return
+     * @return Direccion
      */
     public String getDireccion();
 
     /**
      * Retorna el teléfono de la sucursal
      *
-     * @return
+     * @return Teléfono
      */
     public String getTelefono();
 
     /**
      * Retorna el nombre de la sucursal
      *
-     * @return
+     * @return Nombre
      */
     public String getNombre();
 
     /**
      * Retorna el barrio de la sucursal
      *
-     * @return
+     * @return Barrio
      */
     public String getBarrio();
 
     /**
      * Retorna la ciudad de la sucursal
      *
-     * @return
+     * @return Ciudad
      */
     public String getCiudad();
 
     /**
      * Retorna el arbol de productos de la sucursal
      *
-     * @return
+     * @return TArbolBB
      */
     public TArbolBB<Producto> getArbolProductos();
 
     /**
      * Inserta un producto en el arbol de productos de la sucursal la sucursal
      *
-     * @param unProducto
+     * @param unProducto Producto a insertar
      */
     public void insertarProducto(Producto unProducto);
 
     /**
      * Agrega stock a un producto de la sucursal
      *
-     * @param etiqueta
-     * @param cantidad
+     * @param etiqueta Etiqueta a la cual agregar stock
+     * @param cantidad Cantidad a agregar
      * @return True o False dependiendo si se agregó o no el stock
      */
     public Boolean agregarStock(Comparable etiqueta, Integer cantidad);
@@ -66,8 +66,8 @@ public interface ISucursal {
     /**
      * Vende un producto en la sucursal
      *
-     * @param etiqueta
-     * @param cantidad
+     * @param etiqueta Etiqueta sobre la cual realizar la venta
+     * @param cantidad Cantidad a vender
      */
     public void vender(Comparable etiqueta, Integer cantidad);
 
@@ -75,8 +75,8 @@ public interface ISucursal {
      * Método que dice si un producto se puede vender en la sucursal (si tiene
      * el stock suficiente)
      *
-     * @param etiqueta
-     * @param cantidad
+     * @param etiqueta Etiqueta sobre la cual verificar si se puede vender
+     * @param cantidad Cantidad que se intenta vender
      * @return True o False dependiendo si se puede vender o no
      */
     public boolean sePuedeVender(Comparable etiqueta, Integer cantidad);
@@ -84,7 +84,7 @@ public interface ISucursal {
     /**
      * Método que busca un producto en el arbol de productos de la sucursal
      *
-     * @param etiqueta
+     * @param etiqueta Etiqueta sobre la cual realizar la búsqueda
      * @return Devuelve el producto si lo encuentra y sino devuelve null
      */
     public Producto buscarPorCodigo(Comparable etiqueta);
@@ -92,7 +92,7 @@ public interface ISucursal {
     /**
      * Método que elimina un producto de la sucursal
      *
-     * @param clave
+     * @param clave Clave sobre la cual realizar la eliminacion
      * @return True o False dependiendo si se eliminó o no el producto.
      */
     public boolean eliminarProducto(Comparable clave);
